9cd26a776115c48b59c68447d8b4afab722af2a2
Cristian Arce <cristian.arce@gorillalogic.com>
2020-02-18 14:01:41 -0600

    [SC-14] Add config entries for envs, unite testing, styles

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyBFldEwbfLg-hELBIgPL5vzThkOp_lIRww',
+    authDomain: 'gl-shoppingcart.firebaseapp.com',
+    databaseURL: 'https://gl-shoppingcart.firebaseio.com',
+    projectId: 'gl-shoppingcart',
+    storageBucket: 'gl-shoppingcart.appspot.com',
+    messagingSenderId: '429007400535',
+    appId: '1:429007400535:web:7a8e7f53b2e41b58e4bd12',
+    measurementId: 'G-5NLWRW7X8P',
+  },
 };
